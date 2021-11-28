b95c9dd59f0d9e79704225b7d9d366a834389fd7
BenPaulat <benjamin.d.paulat@gmail.com>
2021-11-27 18:46:29 -0600

    added Update route for category-routes and added 404 error handling

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,6 +41,7 @@ router.get('/:id', (req, res) => {
   .then(dbCategoryData => {
     if (!dbCategoryData) {
       res.status(404).json({ message: 'No category found with this id' });
+      return;
     }
     res.json(dbCategoryData);
   })
@@ -54,7 +55,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   Category.create({
-    category_id: req.body.category_id
+    category_name: req.body.category_name
   })
   .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
@@ -66,10 +67,23 @@ router.post('/', (req, res) => {
 // PUT route
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update({
-
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(dbCategoryData);
   })
-  .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
