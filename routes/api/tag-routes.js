60d0595588c6bb033d08baa95cc13bdb027498ab
BenPaulat <benjamin.d.paulat@gmail.com>
2021-12-12 17:35:19 -0600

    added find one route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -9,7 +9,6 @@ router.get('/', (req, res) => {
   Tag.findAll({
     attributes: ['id', 'tag_name'],
     include: [
-      Category,
       {
         model: Product,
         through: ProductTag
@@ -26,6 +25,29 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
